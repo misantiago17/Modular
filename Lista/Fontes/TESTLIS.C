b15e151e0e4b983021175b2d93ce86f5d9e07385
gbusquim <gbtatu@hotmail.com>
2017-09-27 21:59:31 -0300

    funcao criar correta(inacabado)

--- Lista/Fontes/TESTLIS.C
+++ Lista/Fontes/TESTLIS.C
@@ -128,20 +128,19 @@ LIS_tppLista   vtListas[ DIM_VT_LISTA ] ;
          else if ( strcmp( ComandoTeste , CRIAR_LISTA_CMD ) == 0 )
          {
 
-            numLidos = LER_LerParametros( "i" ,
-                       &inxLista ) ;
+            numLidos = LER_LerParametros( "ii" ,
+                       &inxLista, &CondRet ) ;
 
-            if ( ( numLidos != 1 )
+            if ( ( numLidos != 2 )
               || ( ! ValidarInxLista( inxLista , VAZIO )))
             {
                return TST_CondRetParm ;
             } /* if */
 
-            vtListas[ inxLista ] =
-                 LIS_CriarLista( DestruirValor ) ;
+            CondRetEsp=LIS_CriarLista( DestruirValor,&vtListas[inxLista]) ;
 
-            return TST_CompararPonteiroNulo( 1 , vtListas[ inxLista ] ,
-               "Erro em ponteiro de nova lista."  ) ;
+            return TST_CompararInt( CondRetEsp , CondRet ,
+               "Erro e=na condicao de retorno ao criar a lista"  ) ;
 
          } /* fim ativa: Testar CriarLista */
 
