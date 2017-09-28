3f8a29b73430cfbf09c59b13e04b88b4f02915b9
awybin <awybin@gmail.com>
2017-09-27 21:49:28 -0300

    inicio do desenvolvimento de grafos

---
+++ Grafo/GRAFO.c
@@ -0,0 +1,18 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: GRA  Grafo generico
+*
+*  Arquivo gerado:              GRAFO.c
+*  Letras identificadoras:      GRA
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*  Arquivo da base de software: ????????????????
+*
+*  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
+*  Gestor:  LES/DI/PUC-Rio
+*  Autores: Gabriel Busquim (gb), Michelle Santiago (ms), Renan Moreira (rm)
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       rm   27/09/2017 início desenvolvimento
+*
+***************************************************************************/
---
+++ Grafo/GRAFO.h
@@ -0,0 +1,24 @@
+#if ! defined( GRAFO_ )
+#define GRAFO_
+/***************************************************************************
+*
+*  $MCD Módulo de definição: GRA  Grafo generico
+*
+*  Arquivo gerado:              GRAFO.h
+*  Letras identificadoras:      GRA
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*  Arquivo da base de software: ???????????????????
+*
+*  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
+*  Gestor:  LES/DI/PUC-Rio
+*  Autores: Gabriel Busquim (gb), Michelle Santiago (ms), Renan Moreira (rm)
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       rm   27/09/2017 início desenvolvimento
+*
+*  $ED Descrição do módulo
+*     Implementa grafos genericos
+*
+***************************************************************************/
